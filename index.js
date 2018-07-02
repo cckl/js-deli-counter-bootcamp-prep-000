72b93070aaa9ec44de56acb9b159000d5f3ed6c2
Christina L <clingc7@gmail.com>
2018-07-02 03:08:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,47 +1,64 @@
 var katzDeli = [ ]
 
-/* function takeANumber(katzDeliLine, name) {
-  katzDeli.push(name);
-  return "Welcome, " + name + ". You are number " + katzDeli.indexOf(name)  + " in line."
-} */
 
-function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
-  return `Welcome, ${name}. You are number ${katzDeli.length} in line.`
+function takeANumber(katzDeliLine, name) {
+  var position = katzDeliLine.length + 1
+  katzDeliLine.push(name);
+  return "Welcome, " + name + ". You are number " + position + " in line."
 }
 
 function nowServing(line) {
   if (line.length === 0) {
-    return("There is nobody waiting to be served!")
-  } else {
-      return "Currently serving " + line.shift() + "."
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return "Currently serving " + line.shift() + "."
   }
 }
-//How come no loop here?
 
 
-function currentLine(line) {
-  var katzDeliLine = [];
-  let i = 0;
-  while (i < line.length) {
-    katzDeliLine.push(" " + (i + 1) + ". " + line[i]);
-    i++;
-  } if (line.length === 0) {
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
     return "The line is currently empty."
-  } else {
-    return "The line is currently:" + katzDeliLine
+  }
+  else {
+    for (var i = 0; i < katzDeliLine.length; i++) {
+      var name = katzDeliLine[i]
+      var number = i + 1
+      katzDeliLine.push(` ${number}. ${name}`);
+    }
+    return `The line is currently: ${katzDeliLine}`
   }
 }
 
-/* function currentLine(line) {
-  var katzDeliLine = [];
-  if (line.length === 0) {
-    return ("The line is currently empty.")
-  } else {
-    for (var i = 0; i < line.length; i++) {
-      katzDeliLine += (i + 1) + ". " + line[i] + ", "
+
+/* function currentLine(katzDeliLine) {
+  for (var i = 0; i < katzDeliLine.length; i++) {
+      var name = katzDeliLine[i]
+      var number = i + 1
+      katzDeli.push(" " + number + "." + name);
+   if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+   }
+   else {
+      return "The line is currently: " + katzDeliLine
     }
-    return "The line is currently: " + katzDeliLine
   }
 } */
-//Above: how to remove the comma
+  
+
+  
+ /* var katzDeliLine = [];
+    if (line.length === 0) {
+    return "The line is currently empty." 
+    } 
+    else {
+     for (var i = 0; i < line.length; i++) {
+      var position = katzDeli[i] + 1 
+      var name = katzDeli[i]
+      katzDeliLine.push((katzDeli[i] + 1) + ". " + name)
+    }
+     return "The line is currently:" + katzDeliLine
+  } 
+} */
+
